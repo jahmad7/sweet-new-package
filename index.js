5de179bc134b5fef9f8fdf8b6c32b4a363b794f5
Junaid <jahmad095@gmail.com>
2019-07-22 15:35:14 -0400

    new fix

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-module.exports = {
-    name: `»-(¯"·.·"¯)->{props.name}<-(¯"·.·"¯)-«`
+module.exports = props => {
+    return `»-(¯"·.·"¯)->${props}<-(¯"·.·"¯)-«`
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+### Trying to publish a package
+
+```
+npm i sweet-new-package
+```
+
+makes a cool border for your name when its passed in as a prop
